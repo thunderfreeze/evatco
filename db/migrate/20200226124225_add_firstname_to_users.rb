58a592af9c802f152f6e12fe4602b2636597d469
debian <nathbbb4@gmail.com>
2020-02-26 17:18:38 +0100

    fix

--- db/migrate/20200226124225_add_firstname_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFirstnameToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :firstname, :string
-    add_index :users, :firstname, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_124258) do
+ActiveRecord::Schema.define(version: 2020_02_25_175707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
 
   create_table "questionnaires", force: :cascade do |t|
     t.bigint "user_id"
@@ -45,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_124258) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -52,14 +42,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_124258) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "firstname"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["firstname"], name: "index_users_on_firstname", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "questionnaires", "users"
