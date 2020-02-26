cb3f6429826d7781dfa63e5bfcd4443ce309bc4b
debian <nathbbb4@gmail.com>
2020-02-26 19:12:59 +0100

    Remplissage obligatoire questionnaire

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,3 +1,6 @@
 class Questionnaire < ApplicationRecord
   belongs_to :user
+
+  validates :question1, :question2, :question3, :question4, :question5, :importance1, :importance2, :importance3, :importance4, :importance5,  presence: true
+
 end
--- db/migrate/20200225175707_create_questionnaires.rb
+++ db/migrate/20200225175707_create_questionnaires.rb
@@ -2,16 +2,16 @@ class CreateQuestionnaires < ActiveRecord::Migration[5.2]
   def change
     create_table :questionnaires do |t|
       t.references :user, foreign_key: true
-      t.text :question1
-      t.text :question2
-      t.text :question3
-      t.text :question4
-      t.text :question5
-      t.integer :importance1
-      t.integer :importance2
-      t.integer :importance3
-      t.integer :importance4
-      t.integer :importance5
+      t.text :question1   
+      t.text :question2   
+      t.text :question3   
+      t.text :question4   
+      t.text :question5   
+      t.integer :importance1   
+      t.integer :importance2   
+      t.integer :importance3   
+      t.integer :importance4   
+      t.integer :importance5   
 
       t.timestamps
     end
